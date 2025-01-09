0354c2a5eb11edf6bea0e143275977a2c46ba3fc
Vinicius R. da Silva <silva.viniciusr@gmail.com>
2025-01-09 12:32:50 -0300

    Decrease configMINIMAL_STACK_SIZE to original 85 words, or bytes in this architecture.
    Adjust com_task STACK_SIZE to compensate for this decrease.
    
    Applcation is stable.

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -44,7 +44,7 @@
 #define configCPU_CLOCK_HZ			( ( unsigned long ) 16000000 )
 #define configTICK_RATE_HZ			( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES		( 4 )
-#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 96 )
+#define configMINIMAL_STACK_SIZE	( ( unsigned short ) 85 )
 #define configTOTAL_HEAP_SIZE		( (size_t ) ( 1500 ) )
 #define configMAX_TASK_NAME_LEN		( 8 )
 #define configUSE_TRACE_FACILITY	0
--- src/com_task.c
+++ src/com_task.c
@@ -32,7 +32,7 @@
 
 #define serialBUFFER_LEN                16
 #define mBUFFER_LEN                     20
-#define mSTACK_SIZE                     configMINIMAL_STACK_SIZE + mBUFFER_LEN * 2
+#define mSTACK_SIZE                     configMINIMAL_STACK_SIZE + mBUFFER_LEN * 2 + 6
 
 #define mNO_BLOCK                       (TickType_t) 0
 #define mDELAY_MS(x)                    (TickType_t) (x / portTICK_PERIOD_MS)
