4a26ef9f29ef2c4f1f1400b93496749bfce8892e
Vinicius R. da Silva <silva.viniciusr@gmail.com>
2025-01-15 14:01:09 -0300

    Configure mqtt_task.h

--- include/mqtt_task.h
+++ include/mqtt_task.h
@@ -27,45 +27,6 @@
 #ifndef DEMO_CONFIG_H
 #define DEMO_CONFIG_H
 
-/**************************************************/
-/******* DO NOT CHANGE the following order ********/
-/**************************************************/
-
-/* Include logging header files and define logging macros in the following order:
- * 1. Include the header file "logging_levels.h".
- * 2. Define the LIBRARY_LOG_NAME and LIBRARY_LOG_LEVEL macros depending on
- * the logging configuration for DEMO.
- * 3. Include the header file "logging_stack.h", if logging is enabled for DEMO.
- */
-
-#include "logging_levels.h"
-
-/* Logging configuration for the Demo. */
-#ifndef LIBRARY_LOG_NAME
-    #define LIBRARY_LOG_NAME    "MQTTDemo"
-#endif
-
-#ifndef LIBRARY_LOG_LEVEL
-    #define LIBRARY_LOG_LEVEL    LOG_INFO
-#endif
-
-/* Prototype for the function used to print to console on Windows simulator
- * of FreeRTOS.
- * The function prints to the console before the network is connected;
- * then a UDP port after the network has connected. */
-extern void vLoggingPrintf( const char * pcFormatString,
-                            ... );
-
-/* Map the SdkLog macro to the logging function to enable logging
- * on Windows simulator. */
-#ifndef SdkLog
-    #define SdkLog( message )    vLoggingPrintf message
-#endif
-
-#include "logging_stack.h"
-
-/************ End of logging configuration ****************/
-
 /**
  * @brief The MQTT client identifier used in this example.  Each client identifier
  * must be unique so edit as required to ensure no two clients connecting to the
@@ -78,6 +39,7 @@ extern void vLoggingPrintf( const char * pcFormatString,
  *
  * #define democonfigCLIENT_IDENTIFIER				"insert here."
  */
+#define democonfigCLIENT_IDENTIFIER              "ARDUINO_UNO_R3"
 
 
 /**
@@ -88,6 +50,7 @@ extern void vLoggingPrintf( const char * pcFormatString,
  *
  * #define democonfigMQTT_BROKER_ENDPOINT				"insert here."
  */
+#define democonfigMQTT_BROKER_ENDPOINT           "192.168.0.235"
 
 
 /**
@@ -95,6 +58,7 @@ extern void vLoggingPrintf( const char * pcFormatString,
  *
  * #define democonfigMQTT_BROKER_PORT					( insert here. )
  */
+#define democonfigMQTT_BROKER_PORT               "1883"
 
 
 /**
@@ -108,6 +72,6 @@ extern void vLoggingPrintf( const char * pcFormatString,
 /**
  * @brief Size of the network buffer for MQTT packets.
  */
-#define democonfigNETWORK_BUFFER_SIZE    ( 1024U )
+#define democonfigNETWORK_BUFFER_SIZE    ( 128U )
 
 #endif /* DEMO_CONFIG_H */
