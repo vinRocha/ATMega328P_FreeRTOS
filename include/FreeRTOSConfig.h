787706561353cc797da169599b3209462bc84a4a
Vinicius R. da Silva <silva.viniciusr@gmail.com>
2025-01-27 14:50:02 -0300

    Restore FreeRTOS heap to 1014 given there is no need right now to use its maximum size

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -45,7 +45,7 @@
 #define configTICK_RATE_HZ                  ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES                ( 3 )
 #define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 80 )
-#define configTOTAL_HEAP_SIZE               ( (size_t ) ( 1198) )
+#define configTOTAL_HEAP_SIZE               ( (size_t ) ( 1014) )
 #define configMAX_TASK_NAME_LEN             ( 4 )
 #define configUSE_TRACE_FACILITY            0
 #define configUSE_16_BIT_TICKS              1
